843048c0e83f125b8ed81b8188ea19730c31286e
Wesley Dickens <me@wesleydickens.com>
2024-02-15 16:00:01 -0500

    streamlit change

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import os
 # Load data and models
 @st.cache
 def load_data():
-#     cleaned_df = pd.read_csv('cleaned_df.csv')
+    cleaned_df = pd.read_csv('cleaned_df')
     user_encoder = joblib.load('user_encoder.joblib')
     item_encoder = joblib.load('item_encoder.joblib')
     return cleaned_df, user_encoder, item_encoder
